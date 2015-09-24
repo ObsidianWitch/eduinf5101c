ef46aff39a58453ea375939053eb06811bc550fd
Obside <obsiagae@gmail.com>
2015-09-24 03:59:54 +0200

    clean Couleur

--- src/Couleur.cs
+++ src/Couleur.cs
@@ -7,34 +7,6 @@ namespace ImageSynthesis {
         /// R,G,B in [0,1]
         public float R, G, B;
 
-        public void From255(byte RR, byte GG, byte BB) {
-            R = (float)(RR / 255.0);
-            G = (float)(GG / 255.0);
-            B = (float)(BB / 255.0);
-        }
-
-        static public  void Transpose(ref Couleur cc, System.Drawing.Color c) {
-            cc.R = (float) (c.R / 255.0);
-            cc.G = (float) (c.G / 255.0);
-            cc.B = (float) (c.B / 255.0);
-        }
-
-        public void check() {
-            if (R > 1.0) R = 1.0f;
-            if (G > 1.0) G = 1.0f;
-            if (B > 1.0) B = 1.0f;
-        }
-
-        public Color To255() {
-            check();
-            
-            byte r255 = (byte) (R * 255);
-            byte g255 = (byte) (G * 255);
-            byte b255 = (byte) (B * 255);
-            
-            return Color.FromArgb(r255, g255, b255);
-        }
-
         public Couleur(float R, float G, float B) {
             this.R = R;
             this.G = G;
@@ -47,10 +19,34 @@ namespace ImageSynthesis {
             this.B = c.B;
         }
 
+        public void From255(byte r255, byte g255, byte b255) {
+            R = (float) (r255 / 255.0);
+            G = (float) (g255 / 255.0);
+            B = (float) (b255 / 255.0);
+        }
+
+        public Color To255() {
+            check();
+            
+            byte r255 = (byte) (R * 255);
+            byte g255 = (byte) (G * 255);
+            byte b255 = (byte) (B * 255);
+            
+            return Color.FromArgb(r255, g255, b255);
+        }
+
         /// Useful for bump mapping
         public float GreyLevel() {
-            return (R + B + G) / 3.0f;
+            return (R + G + B) / 3.0f;
         }
+
+        private void check() {
+            if (R > 1.0) R = 1.0f;
+            if (G > 1.0) G = 1.0f;
+            if (B > 1.0) B = 1.0f;
+        }
+
+        // Operators
 
         public static Couleur operator +(Couleur a, Couleur b) {
             return new Couleur(a.R + b.R, a.G + b.G, a.B + b.B);
