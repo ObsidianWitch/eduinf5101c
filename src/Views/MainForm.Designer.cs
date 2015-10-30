7ac4b129aeaba323bae727beaf32bf072cb80154
Obside <obsiagae@gmail.com>
2015-10-30 16:18:05 +0100

    UI - choose Renderer

--- src/Views/MainForm.Designer.cs
+++ src/Views/MainForm.Designer.cs
@@ -3,6 +3,7 @@
         
         private System.Windows.Forms.Button RenderButton;
         private System.Windows.Forms.CheckBox SlowModeCheckbox;
+        private System.Windows.Forms.ComboBox RendererComboBox;
         private System.ComponentModel.IContainer components = null;
         
         protected override void Dispose(bool disposing) {
@@ -15,10 +16,11 @@
         private void InitializeComponent() {
             this.RenderButton = new System.Windows.Forms.Button();
             this.SlowModeCheckbox = new System.Windows.Forms.CheckBox();
+            this.RendererComboBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
-            //
+            // 
             // RenderButton
-            //
+            // 
             this.RenderButton.Location = new System.Drawing.Point(12, 12);
             this.RenderButton.Name = "RenderButton";
             this.RenderButton.Size = new System.Drawing.Size(75, 23);
@@ -26,9 +28,9 @@
             this.RenderButton.Text = "Render";
             this.RenderButton.UseVisualStyleBackColor = true;
             this.RenderButton.Click += new System.EventHandler(this.RenderButtonClick);
-            //
+            // 
             // SlowModeCheckbox
-            //
+            // 
             this.SlowModeCheckbox.AutoSize = true;
             this.SlowModeCheckbox.Checked = true;
             this.SlowModeCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
@@ -39,19 +41,31 @@
             this.SlowModeCheckbox.Text = "Slow mode";
             this.SlowModeCheckbox.UseVisualStyleBackColor = true;
             this.SlowModeCheckbox.CheckedChanged += new System.EventHandler(this.SlowModeToggle);
-            //
+            // 
+            // RendererComboBox
+            // 
+            this.RendererComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.RendererComboBox.FormattingEnabled = true;
+            this.RendererComboBox.Location = new System.Drawing.Point(177, 12);
+            this.RendererComboBox.Name = "RendererComboBox";
+            this.RendererComboBox.Size = new System.Drawing.Size(121, 21);
+            this.RendererComboBox.TabIndex = 5;
+            this.RendererComboBox.SelectedIndexChanged += new System.EventHandler(this.RendererChanged);
+            // 
             // MainForm
-            //
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(821, 547);
+            this.Controls.Add(this.RendererComboBox);
             this.Controls.Add(this.SlowModeCheckbox);
             this.Controls.Add(this.RenderButton);
             this.Name = "MainForm";
             this.Text = "INF-5101C - Projet";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
         
         private void InitializeCanvas(Canvas canvas) {
@@ -66,5 +80,6 @@
             ResumeLayout(false);
             PerformLayout();
         }
+
     }
 }
--- src/Views/MainForm.cs
+++ src/Views/MainForm.cs
@@ -3,6 +3,7 @@ using System.Windows.Forms;
 using ImageSynthesis.Scenes;
 using ImageSynthesis.Renderers;
 using ImageSynthesis.Lights;
+using System.Collections.Generic;
 
 namespace ImageSynthesis.Views {
     
@@ -10,8 +11,10 @@ namespace ImageSynthesis.Views {
         
         public const int CANVAS_WIDTH = 800;
         public const int CANVAS_HEIGHT = 500;
-        
-        private Renderer Renderer;
+
+        private V3 CameraPos;
+        private Dictionary<string, Renderer> Renderers;
+        private Renderer CurrentRenderer;
         
         public MainForm() {
             InitializeComponent();
@@ -19,25 +22,55 @@ namespace ImageSynthesis.Views {
             Canvas canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
             InitializeCanvas(canvas);
             
-            V3 cameraPos = new V3(CANVAS_WIDTH/2, -1000, CANVAS_HEIGHT/2);
+            CameraPos = new V3(CANVAS_WIDTH/2, -1000, CANVAS_HEIGHT/2);
             
-            Scene scene = new Scene(new PhongIllumination(cameraPos));
+            Scene scene = new Scene(new PhongIllumination(CameraPos));
             DefaultScene.PopulateDL(scene);
-            
-            Renderer = new Raytracing(
+
+            InitializeRenderers(canvas, scene);
+        }
+
+        private void InitializeRenderers(Canvas canvas, Scene scene) {
+            Renderers = new Dictionary<string, Renderer>();
+
+            Renderer zbuffer = new ZBuffer(
+                canvas: canvas,
+                scene: scene
+            );
+            Renderers.Add("ZBuffer", zbuffer);
+
+            Renderer raycasting = new Raycasting(
                 canvas:    canvas,
                 scene:     scene,
-                cameraPos: cameraPos,
+                cameraPos: CameraPos
+            );
+            Renderers.Add("Raycasting", raycasting);
+
+            Renderer raytracing = new Raytracing(
+                canvas:    canvas,
+                scene:     scene,
+                cameraPos: CameraPos,
                 maxDepth:  10
             );
+            Renderers.Add("Raytracing", raytracing);
+
+            RendererComboBox.DataSource = new BindingSource(Renderers, null);
+            RendererComboBox.DisplayMember = "Key";
+            RendererComboBox.ValueMember = "Value";
         }
 
         private void RenderButtonClick(object sender, EventArgs e) {
-            Renderer.Render();
+            CurrentRenderer.Render();
         }
 
         private void SlowModeToggle(object sender, EventArgs e) {
-            Renderer.Canvas.SwitchDisplayMode(SlowModeCheckbox.Checked);
+            CurrentRenderer.Canvas.SwitchDisplayMode(SlowModeCheckbox.Checked);
+        }
+
+        private void RendererChanged(object sender, EventArgs e) {
+            var selectedItem = (KeyValuePair <string, Renderer>) RendererComboBox.SelectedItem;
+            CurrentRenderer = selectedItem.Value;
+
         }
 
     }
